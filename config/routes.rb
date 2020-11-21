3dd0fd1f26fbea7eb6731da2e7f84df1727dabad
Marcus Rocha <sif.marcusvinicius@gmail.com>
2020-11-20 23:15:33 -0300

    Start to config the device's views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   
   root to: 'home#index'
   
-  mount_devise_token_auth_for 'User', at: 'auth'
+  devise_for :users
+  #mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
